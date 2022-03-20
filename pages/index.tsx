c5a5856703d60f2cbecaa16c1126f40929c8a932
gnihker <sadapa.chueng@gmail.com>
2022-03-20 21:05:48 +0700

    promotion banner edited

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,8 +74,8 @@ const Home: NextPage = () => {
         </div>
         <div className='promotion-banner'>
           <div className={'promotion-banner-left'} id={'flex-9'}>
-            <RectBanner size='small' />
-            <RectBanner size='small' />
+            <div id='promotion-banner-small'><RectBanner size='small' /></div>
+            <div id='promotion-banner-small'><RectBanner size='small' /></div>
           </div>
           <div className={'promotion-banner-right'} id={'flex-10'}>
             <RectBanner size='medium' />
